460703b30648b7a1f3c39adaa7e4f2089f1a818a
Kirill Voronchihin <skiletoons@mail.ru>
2022-03-06 16:14:24 +0700

    Тест на возвраст 2

--- sss.js
+++ sss.js
@@ -1,13 +1,14 @@
 function show() {
 	let age = parseInt(document.getElementById('age').value)
+	let div = document.getElementById('mes')
 
-	if (age < 10){
-		console.log('Вы ребенок')
+	if (age < 10) {
+		div.innerHTML = '<h1>Вы ребенок</h1>'
 	}
 else if (age < 17) {
-	console.log('Вы подросток')
+	div.innerHTML = '<h1>Вы подросток</h1>'
 }
-else{
-	console.log('Вы взрослый')
-}
+else {
+	div.innerHTML ='<h1>Вы взрослый</h1>'
+    }
 }
