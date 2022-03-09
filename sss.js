1e5f796b8f19d2bdf67347af885599b8e7c82a29
Kirill Voronchihin <skiletoons@mail.ru>
2022-03-09 21:53:39 +0700

    Массив с рандомными числами

--- sss.js
+++ sss.js
@@ -1,14 +1,6 @@
-function show() {
-	let age = parseInt(document.getElementById('age').value)
-	let div = document.getElementById('mes')
-
-	if (age < 10) {
-		div.innerHTML = '<h1>Вы ребенок</h1>'
-	}
-else if (age < 17) {
-	div.innerHTML = '<h1>Вы подросток</h1>'
+let arr = []
+const n = 10
+for (let i = 0; i < n; i++){
+	arr[i] = Math.floor(Math.random()*10)
 }
-else {
-	div.innerHTML ='<h1>Вы взрослый</h1>'
-    }
-}
+console.log(arr)
