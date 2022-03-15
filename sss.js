10f4be5f9fae1947087ea16b24203780145c1177
Kirill Voronchihin <skiletoons@mail.ru>
2022-03-15 22:35:08 +0700

    Student

--- sss.js
+++ sss.js
@@ -1,6 +1,11 @@
-let arr = []
-const n = 10
-for (let i = 0; i < n; i++){
-	arr[i] = Math.floor(Math.random()*10)
+let students = [
+	{'id': 1, 'name': 'Данила', 'surname': 'Алферов'},
+	{'id': 2, 'name': 'Тихон', 'surname': 'Балаганский'},
+	{'id': 3, 'name': 'Константин', 'surname': 'Бобровский'},
+	{'id': 4, 'name': 'Владимир', 'surname': 'Бурыкин'},
+	{'id': 5, 'name': 'Семен', 'surname': 'Велигжанин'}
+]
+
+for ( i = 0; i < students.length; i++) {
+	console.log(students[i].surname+' '+students[i].name)
 }
-console.log(arr)
