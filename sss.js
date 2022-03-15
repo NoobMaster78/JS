5aca530a7057c70f53a873d396bb6b505eb29399
Kirill Voronchihin <skiletoons@mail.ru>
2022-03-16 00:17:26 +0700

    Список с кнопками

--- sss.js
+++ sss.js
@@ -5,7 +5,20 @@ let students = [
 	{'id': 4, 'name': 'Владимир', 'surname': 'Бурыкин'},
 	{'id': 5, 'name': 'Семен', 'surname': 'Велигжанин'}
 ]
+let current = 0 
 
-for ( i = 0; i < students.length; i++) {
-	console.log(students[i].surname+' '+students[i].name)
+// загрузка по ид
+function load_stedent(id) {
+	document.getElementsById('name').value = students[id].name
+	document.getElementsById('surname').value = students[id].surname
+}
+
+//следующий студент
+function next() {
+	load_stedent(current)
+	current++
+
+	if (current === students.length) {
+		document.getElementsById('bth_next').disabled - true
+	}
 }
